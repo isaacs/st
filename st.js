32ec198126efe2327f80f5072c04383d646caa63
isaacs <i@izs.me>
2012-08-09 23:07:36 -0700

    getting closer to sorta working
    Problems:
    - Really need to sort out how the caching all works. It's caching
    fd's, contents, stats, and should probably also cache readdir results,
    but this is all over the place
    - General structure is sloppy as hell.  It's starting to seem like an
    object-oriented approach may be justified here, if only to have a
    single context-object that can be passed around, instead of these
    methods with tons of arguments
    
    But it's pretty fast, and the external-facing API is fairly nice.

--- st.js
+++ st.js
@@ -7,6 +7,7 @@ var fs = require('fs')
 var url = require('url')
 var zlib = require('zlib')
 var Neg = require('negotiator')
+var http = require('http')
 
 function st (opt) {
   var p, u
@@ -27,24 +28,26 @@ function st (opt) {
   if (u.charAt(0) !== '/') u = '/' + u
 
   // default to 10 minutes
-  var cacheExpiry = opts.cacheExpiry
+  var cacheExpiry = opt.cacheExpiry
   if (isNaN(cacheExpiry)) cacheExpiry = 1000 * 60 * 10
 
   // fd cache
-  var fdCacheSize = +opts.fdCacheSize
+  var fdCacheSize = +opt.fdCacheSize
   if (isNaN(fdCacheSize) || fdCacheSize < 0) {
     fdCacheSize = 1024
   }
   var fdCache, statCache
   if (fdCacheSize > 0) {
-    var fdCacheOpts = {
+    fdCache = LRU({
       max: fdCacheSize,
       dispose: function (key, fd) {
         fs.close(fd, function () {})
       }
-    }
-    fdCache = new LRU(fdCacheOpts)
-    statCache = new LRU(fdCacheOpts)
+    })
+    statCache = new LRU({
+      max: fdCacheSize,
+      maxAge: cacheExpiry
+    })
   } else {
     // fake cache
     fdCache = statCache = {
@@ -55,7 +58,7 @@ function st (opt) {
   }
 
   // content cache
-  var cacheSize = opts.cacheSize
+  var cacheSize = opt.cacheSize
   if (isNaN(cacheSize)) cacheSize = 0
   var contentCache
   if (cacheSize) {
@@ -73,7 +76,6 @@ function st (opt) {
   return mount
   function mount (req, res, next) {
     var ru = url.parse(req.url).pathname.replace(/\/\/+/g, '/')
-    ru = ru.replace(/\/+$/, '')
     var pos = ru.indexOf(u)
     // not something we care about
     if (pos !== 0) {
@@ -81,10 +83,18 @@ function st (opt) {
       return false
     }
 
+    // don't allow dot-urls by default, unless explicitly allowed.
+    if (!opt.dot && ru.match(/(^|\/)\./)) {
+      res.statusCode = 403
+      res.end('Forbidden')
+      return true
+    }
+
     // At this point, we're taking the request, because it's into the mount.
     // make sure to return true!
 
-    var pru = path.resolve(p, ru)
+    var pru = path.join(p, path.join('/', ru))
+    pru = pru.replace(/\/+$/, '')
     var etag = etags[pru]
     if (etag && etag === req.headers['if-none-match']) {
       res.statusCode = 304
@@ -96,8 +106,8 @@ function st (opt) {
     // does that play nice with gzip?  the spec is kinda dense.
 
     var gz = false
-    if (opts.gz !== false) {
-      var neg = req.negotiator || new Negotiator(req)
+    if (opt.gz !== false) {
+      var neg = req.negotiator || new Neg(req)
       gz = neg.preferredEncoding('gzip', 'identity') === 'gzip'
     }
 
@@ -107,6 +117,7 @@ function st (opt) {
       if (cached) {
         res.statusCode = 200
         res.setHeader('etag', cached.etag)
+        res.setHeader('mime-type', mime.lookup(path.extname(pru)))
         if (gz) {
           res.setHeader('content-encoding', 'gzip')
           res.setHeader('content-length', cached.gz.length)
@@ -127,29 +138,74 @@ function st (opt) {
       // the fd is opened now, and in the cache
       // need to get the fstat
       fstat(fd, pru, statCache, function (er, stat) {
+        if (er) return error(res, er)
+        if (stat.isDirectory()) {
+          if (opt.autoindex !== false) {
+            // url should always end in /
+            if (!ru.match(/\/$/)) {
+              res.statusCode = 301
+              res.setHeader('location', ru + '/')
+              res.end('Moved: ' + ru + '/')
+              return
+            }
+            return autoindex(ru, pru, opt, res)
+          } else {
+            res.statusCode = 404
+            res.end('not found: ' + u)
+            return
+          }
+        }
+
         // don't bother even attempting to cache if it's too big
         var doCache = contentCache && stat.size < cacheSize
-        serve(res, fd, pru, doCache ? contentCache : null, gz, stat, etags)
+        var cache = fdCache ? contentCache : null
+        serve(res, fd, pru, cache, gz, stat, etags, fdCache)
       })
     })
     return true
   }
 }
 
-function serve (res, fd, pru, cache, gz, stat, etags) {
+function serve (res, fd, pru, cache, gz, stat, etags, fdCache) {
   // kind of wasteful.
   // should probably do the fs stuff manually
   // but if you aren't caching contents, then it's probably because the
   // files are big, and if you are caching contents, then it doesn't
   // matter most of the time anyway.
-  var stream = fs.createReadStream(pru, { fd: fd, end: false })
-  var gzstr = zlip.Gzip()
-  stream.pipe(gzstr)
+  var stream = fs.createReadStream(pru, { start: 0, fd: fd })
+  // we're actually going to re-use the fd repeatedly, so no-op this.
+  stream.destroy = function () {}
+
+  function serveError(er) {
+    console.error('Error', pru, er.stack || er.message)
+    res.destroy()
+    if (cache) cache.del(pru)
+    fdCache.del(pru)
+  }
+
+  stream.on('error', serveError)
+
+  // don't gzip files ending in .gz or .tgz
+  if (!pru.match(/.\.t?gz$/)) {
+    var gzstr = zlib.Gzip()
+    stream.pipe(gzstr)
+    // at this point it's too late to do anything about errors.
+    // just kill the stream.
+    gzstr.on('error', serveError)
+  } else {
+    gz = false
+  }
 
   res.statusCode = 200
+  var etag = [stat.dev, stat.ino, stat.mtime.getTime()].join(':')
+  etag = '"' + etag + '"'
+  res.setHeader('etag', etag)
+  etags[pru] = etag
+  res.setHeader('mime-type', mime.lookup(path.extname(pru)))
 
   if (cache) {
     var ended = 0
+    if (!gzstr) ended++
 
     var clearBufs = []
     stream.on('data', clearBufs.push.bind(clearBufs))
@@ -158,18 +214,19 @@ function serve (res, fd, pru, cache, gz, stat, etags) {
       if (++ended === 2) finishCache()
     })
 
-    var gzBufs = []
-    gzstr.on('data', gzBufs.push.bind(gzBufs))
-    gzstr.on('end', function () {
-      gzBufs = Buffer.concat(gzBufs)
-      if (++ended === 2) finishCache()
-    })
+    if (gzstr) {
+      var gzBufs = []
+      gzstr.on('data', gzBufs.push.bind(gzBufs))
+      gzstr.on('end', function () {
+        gzBufs = Buffer.concat(gzBufs)
+        if (++ended === 2) finishCache()
+      })
+    }
 
     function finishCache () {
       // put it in the contentCache
-      clearBufs.gz = gzBufs
-      clearBufs.etag = [stat.dev, stat.ino, stat.mtime.getTime()].join(':')
-      etags[pru] = etag
+      if (gzstr) clearBufs.gz = gzBufs
+      clearBufs.etag = etag
       cache.set(pru, clearBufs)
     }
   }
@@ -178,9 +235,10 @@ function serve (res, fd, pru, cache, gz, stat, etags) {
   // but at this point, ti would mean reading the whole file before
   // sending anything, since we don't know how big the gzipped will be.
   if (!gz) {
-    res.setHeader(stat.size)
+    res.setHeader('content-length', stat.size)
     stream.pipe(res)
   } else {
+    res.setHeader('content-encoding', 'gzip')
     gzstr.pipe(res)
   }
 }
@@ -227,13 +285,56 @@ function open (path, fdCache, cb_) {
 }
 
 function error (res, er) {
-  res.statusCode = er.code === 'ENOENT' ? 404
-                 : er.code === 'EACCES' ? 403
+  res.statusCode = er.code === 'ENOENT' || er.code === 'EISDIR' ? 404
+                 : er.code === 'EPERM' || er.code === 'EACCES' ? 403
                  : 500
   if (res.error) {
     // pattern of express and ErrorPage
     return res.error(er, res.statusCode)
   }
   res.setHeader('content-type', 'text/plain')
-  res.end(http.STATUS_CODES[res.statusCode] + '\n' er.message)
+  res.end(http.STATUS_CODES[res.statusCode] + '\n' + er.message)
+}
+
+var dirsReading = {}
+function autoindex (url, path, opt, res) {
+  if (dirsReading[path]) {
+    dirsReading[path].push(res)
+    return
+  }
+  dirsReading[path] = [res]
+  fs.readdir(path, function (er, files) {
+    if (!opt.dot) files = files.filter(function (f) {
+      return !f.match(/^\./)
+    })
+    var ress = dirsReading[path]
+    delete dirsReading[path]
+
+    if (er) return ress.forEach(function (res) { error(res, er) })
+
+    html = index(url, files)
+    ress.forEach(function (res) {
+      res.statusCode = 200
+      res.setHeader('content-type', 'text/html')
+      res.setHeader('content-length', html.length)
+      res.end(html)
+    })
+  })
+}
+
+function index (url, files) {
+  var str =
+    '<!doctype html>' +
+    '<html>' +
+    '<head><title>Index of ' + url + '</title></head>' +
+    '<body>' +
+    '<h1>Index of ' + url + '</h1>' +
+    '<hr><pre><a href="../">../</a>\n'
+
+  files.forEach(function (file) {
+    file = file.replace(/"/g, '&quot;')
+    str += '<a href="' + file + '">' + file + '</a>\n'
+  })
+  str += '</pre><hr></body></html>'
+  return new Buffer(str)
 }
