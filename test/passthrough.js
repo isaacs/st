4216223bb874a5a9cdbfd423965a272fa2f0e80e
Christian Howe <coderarity@gmail.com>
2012-10-26 12:12:08 -0500

    fix passthrough tests

--- test/passthrough.js
+++ test/passthrough.js
@@ -13,33 +13,41 @@ var opts = util._extend({
 var mount = st(opts)
 
 test('call next() if passthrough is set', function (t) {
-  var req = { url: '/index.html' }
-  var res = {}
+  var req = { method: 'GET', url: '/doesnotexist.txt', headers: {} }
+  var res = {
+    error: function () {
+      t.end()
+    },
+    setHeader: function () {},
+    end: function () {}
+  }
   t.plan(1)
   function next() {
     t.ok(true, "next called")
+    t.end()
   }
   mount(req, res, next)
 })
 
-opts = util._extend({
+var opts2 = util._extend({
   autoindex: true,
   path: path.resolve(__dirname, './fixtures'),
   url: '/'
 }, global.options || {})
-
-mount = st(opts)
+mount2 = st(opts2)
 
 test('return error if passthrough is not set', function (t) {
-  var req = { url: '/doesnotexist.txt' }
+  var req = { method: 'GET', url: '/doesnotexist.txt', headers: {} }
   var res = {
+    setHeader: function () {},
     error: function () {
       t.ok(true, "error used")
       t.end()
-    }
+    },
+    end: function () {}
   }
   t.plan(1)
-  mount(req, res, function () {
+  mount2(req, res, function () {
     t.end()
   })
 })
