51f977261a4cb2269cda8ae5c8e4256e78d5ab7b
Bryce Gibson <bryce@gibson-consulting.com.au>
2015-05-29 08:42:01 +1000

    Restructure static gzip tests
    To ensure that their is a statically gzipped file for all of the common
    tests as well.

--- test/static-gzip.js
+++ test/static-gzip.js
@@ -10,27 +10,33 @@ var req = basic.req
 var mount = basic.mount
 var stExpect = basic.stExpect
 
-// test to ensure that the request returns the statically gzipped contents
 var test = require('tap').test
 
-test('gzip-static', function (t) {
-  zlib.gzip(stExpect, function(er, gzipped) {
-    if (er) throw er;
-    fs.writeFile('../tmp.txt.gz', gzipped, function(er) {
-      fs.writeFile('../tmp.txt', 'this shouldn\'t be returned', function(er) {
-        if (er) throw er;
-        req('/test/tmp.txt', {'accept-encoding':'gzip'},
-            function (er, res, body) {
-          t.equal(res.statusCode, 200)
-          t.equal(res.headers['content-encoding'], 'gzip')
-          zlib.gunzip(body, function (er, body) {
-            if (er) throw er;
-            t.equal(body.toString(), stExpect)
-            fs.unlink('../tmp.txt.gz', function(er) {
+zlib.gzip(stExpect, function(er, gzipped) {
+  if (er) throw er;
+  fs.writeFile('../st.js.gz', gzipped, function(er) {
+
+    // test to ensure that the request returns the statically gzipped contents
+    test('gzip-static', function (t) {
+      fs.writeFile('../tmp.txt.gz', gzipped, function(er) {
+        fs.writeFile('../tmp.txt', 'this shouldn\'t be returned', function(er) {
+          if (er) throw er;
+          req('/test/tmp.txt', {'accept-encoding':'gzip'},
+              function (er, res, body) {
+            t.equal(res.statusCode, 200)
+            t.equal(res.headers['content-encoding'], 'gzip')
+            zlib.gunzip(body, function (er, body) {
               if (er) throw er;
-              fs.unlink('../tmp.txt', function(er) {
+              t.equal(body.toString(), stExpect)
+              fs.unlink('../tmp.txt.gz', function(er) {
                 if (er) throw er;
-                t.end()
+                fs.unlink('../tmp.txt', function(er) {
+                  if (er) throw er;
+                  fs.unlink('../st.js.gz', function(er) {
+                    if (er) throw er;
+                    t.end()
+                  })
+                })
               })
             })
           })
