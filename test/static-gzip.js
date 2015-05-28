113d72553ae7d570f884468b5309e21e04d801c0
Bryce Gibson <bryce@gibson-consulting.com.au>
2015-05-29 08:52:14 +1000

    Code style

--- test/static-gzip.js
+++ test/static-gzip.js
@@ -14,27 +14,27 @@ var stExpect = basic.stExpect
 var test = require('tap').test
 
 zlib.gzip(stExpect, function(er, gzipped) {
-  if (er) throw er;
+  if (er) throw er
   fs.writeFile('../st.js.gz', gzipped, function(er) {
 
     // test to ensure that the request returns the statically gzipped contents
     test('gzip-static', function (t) {
       fs.writeFile('../tmp.txt.gz', gzipped, function(er) {
         fs.writeFile('../tmp.txt', 'this shouldn\'t be returned', function(er) {
-          if (er) throw er;
+          if (er) throw er
           req('/test/tmp.txt', {'accept-encoding':'gzip'},
               function (er, res, body) {
             t.equal(res.statusCode, 200)
             t.equal(res.headers['content-encoding'], 'gzip')
             zlib.gunzip(body, function (er, body) {
-              if (er) throw er;
+              if (er) throw er
               t.equal(body.toString(), stExpect)
               fs.unlink('../tmp.txt.gz', function(er) {
-                if (er) throw er;
+                if (er) throw er
                 fs.unlink('../tmp.txt', function(er) {
-                  if (er) throw er;
+                  if (er) throw er
                   fs.unlink('../st.js.gz', function(er) {
-                    if (er) throw er;
+                    if (er) throw er
                     t.end()
                   })
                 })
